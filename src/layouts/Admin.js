e2fded3ab45c932fcdbccb7917e18c588a9ecd18
TXLuong <tranxuanluong98@gmail.com>
2021-04-17 11:42:06 +0700

    17-4-21 : backup

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -9,7 +9,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "components/Navbars/Navbar.js";
 import Footer from "components/Footer/Footer.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-// import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
+import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
 import routes from "routes.js";
 
@@ -52,7 +52,7 @@ export default function Admin({ ...rest }) {
   const mainPanel = React.createRef();
   // states and functions
   const [image, setImage] = React.useState(bgImage);
-  const [color, setColor] = React.useState("blue");
+  const [color, setColor] = React.useState("purple");
   const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const handleImageClick = image => {
@@ -125,14 +125,14 @@ export default function Admin({ ...rest }) {
           <div className={classes.map}>{switchRoutes}</div>
         )}
         {getRoute() ? <Footer /> : null}
-        {/* <FixedPlugin
+        <FixedPlugin
           handleImageClick={handleImageClick}
           handleColorClick={handleColorClick}
           bgColor={color}
           bgImage={image}
           handleFixedClick={handleFixedClick}
           fixedClasses={fixedClasses}
-        /> */}
+        />
       </div>
     </div>
   );
--- src/views/TimeTracking/Tracking.js
+++ src/views/TimeTracking/Tracking.js
@@ -1,10 +1,48 @@
 import Camera from '../../components/Cam/Camera';
 import React from 'react';
+import Button from '../../components/CustomButtons/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import {useState} from 'react';
+import CamHtml from '../../components/Cam/CamHtml';
+import authPost from '../../api';
 
+//Add your justify css in your styles const
+const styles = {
+    root: {
+        justifyContent: 'center'
+    }
+};
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+  button:{
+    //   textAlign: 'center',
+    //   display:'block',
+    marginTop : '50px',
+  }, container: {
+    textAlign: 'center',
+  }
+  }));  
+  
 export default function TrackingTime(){
+    const classes = useStyles();
+    const [imageBase64, setImageBase64] = useState("");
+    const [content, setContent] = useState("sdcsdc");
+    const handleClick = ()=>{
+      // detect and crop human face 
+      
+     // send image to server
+
+     console.log("clicked");
+    }
     return (
-        <div>
-            <Camera></Camera>
+        <div className={classes.container}>
+            <CamHtml handleChangeImage = {(imageBase64) => {setImageBase64(imageBase64)}}/>  
+            <img src={imageBase64}></img>
+            <div>
+            <Button className={classes.button} onClick={handleClick}>Turn in</Button>
+            </div>
         </div>
     )
 }
